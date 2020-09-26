77bb1af70e0cdb5b806a123623c7d59f5cd05ce5
giulio ceresetti <giulioceresetti@gmail.com>
2020-09-26 12:30:47 +0200

    input add

--- lib/screen/register_auto_screen.dart
+++ lib/screen/register_auto_screen.dart
@@ -9,20 +9,34 @@ class RegisterAutoScreen extends StatefulWidget {
 
 
 class _RegisterAutoScreenState extends State<RegisterAutoScreen>{
+
+  TextEditingController modelName = TextEditingController();
+  TextEditingController carName= TextEditingController();
+  TextEditingController fuel= TextEditingController();
+  TextEditingController immatricolazione= TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Auto"),),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Benvenuto, qui potrai configurare la tua auto"),
+child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text("Benvenuto, qui potrai configurare la tua auto"),
+      TextField(
+        controller:modelName,textAlign: TextAlign.center,
+      ),            TextField(
+          controller:carName,textAlign: TextAlign.center
+      ),            TextField(
+          controller:fuel,textAlign: TextAlign.center
+      ),            TextField(
+          controller:immatricolazione,textAlign: TextAlign.center
+      ),
+    ]
+),
+      ));
 
-            ]
-        )
-      )
-    );
   }
 
 }
